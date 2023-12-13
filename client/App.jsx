c50bc3a1c0ffc0059f6082cc800845490f5272e0
Casey Doig <cddoigkn@gmail.com>
2023-12-13 14:28:36 -0600

    attempted to add universal background image

--- client/App.jsx
+++ client/App.jsx
@@ -3,35 +3,40 @@ import AppProvider from './utils/AppProvider';
 import { Header, ProtectedRoute, WorkoutDisplay, WorkoutForm, GoalForm } from './components';
 import { HomePage, AuthPage, Logout, PrivatePage } from './pages/'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function App(){
+import weights from './img/weights.jpg';
+export default function App() {
 
   return (
     <AppProvider>
       <BrowserRouter>
         <Header />
         <div className="container pt-5">
+          {/* <div style={{
+            backgroundImage: `url(${weights})`, backgroundRepeat: "no-repeat", backgroundSize: "cover",
+            width: '100vw', height: '100vh'
+          }}>
+          </div> */}
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/auth" element={<AuthPage />} />
-            <Route path="/workoutdisplay" element={<WorkoutDisplay/>} />
+            <Route path="/workoutdisplay" element={<WorkoutDisplay />} />
             <Route path="/private" element={
               <ProtectedRoute>
                 <PrivatePage />
               </ProtectedRoute>
-            }/>
+            } />
 
             <Route path="/workoutform" element={
               <ProtectedRoute>
                 <WorkoutForm />
               </ProtectedRoute>
-            }/>
+            } />
 
             <Route path="/goalform" element={
               <ProtectedRoute>
                 <GoalForm />
               </ProtectedRoute>
-            }/>
+            } />
 
             <Route path="/logout" element={<Logout />} />
           </Routes>

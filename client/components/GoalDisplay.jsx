b667a814412ff676c66dfb9b69ccfe9003007f02
yacob menghistu <yacobabr@gmail.com>
2023-12-13 07:57:45 -0600

    feature-WorkoutDisplay

--- client/components/GoalDisplay.jsx
+++ client/components/GoalDisplay.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useAppCtx } from "../utils/AppProvider";
 
-export default function GoalForm() {
+export default function GoalDisplay() {
 
   const {user} = useAppCtx;
 
-  const [newGoal, displayNewGoal] = useState({weightLoss: 0, weightGain: 0, bmi: 0, totalCalorieGoal: 0, userID: user._id})
+  const [newGoal, displayNewGoal] = useState({weightLoss: 0, weightGain: 0, totalCalorieGoal: 0,})
+  
+  fetch (`/api/user/goals`, {
+    method: "GET",
+    body: JSON.stringify(newGoal),
+  }).then(response => response.json())
+  .then(data =>{
+    console.log(data.payload)
+    displayNewGoal(data.payload)
+  })
+  useEffect(()=>{
+
+  })
 
   return(
     <>
-    <form onSubmit={displayNewGoal}>
-        <label>Weight Loss?: </label>
-        <input type='number' name='weightLoss' value={displayNewGoal.weightLoss} onChange={handleInputChange}/>
-        <div></div>
-        <label>Weight Gain?: </label>
-        <input type='number' name='weightGain' value={displayNewGoal.weightGain} onChange={handleInputChange}/>
-        <div></div>
-        <label>BMI?: </label>
-        <input type='number' name='bmi' value={displayNewGoal.bmi} onChange={handleInputChange}/>
-        <div></div>
-        <label>Total Calories Goals: </label>
-        <input type='number' name='totalCalorieGoal' value={displayNewGoal.totalCalorieGoal} onChange={handleInputChange}/>
-        <div></div>
-        <button>Confirm</button>
-    </form>
+      <Card style={{ width: '18rem' }}>
+      <GoalDisplay variant="flush">
+        <GoalDisplay.Item>weightLoss: </GoalDisplay.Item>
+        <GoalDisplay.Item>weightGain:</GoalDisplay.Item>
+        <GoalDisplay.Item>total Calorie Goal:</GoalDisplay.Item>
+      </GoalDisplay>
+    </Card>
     </>
   )
 }
